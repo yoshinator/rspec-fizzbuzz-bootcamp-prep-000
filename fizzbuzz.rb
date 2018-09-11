fce62f89c023fcbf3e3d5f8a6e4132c9920321ba
yoan ante <yoanante@gmail.com>
2018-09-11 18:55:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(n)
-  if n % 15 == 
-  
+  if n % 15 == 0
+    "FizzBuzz"
+    elsif n % 5 ==0 
+    "Buzz"
+    elsif n % 3 ==0 
+    "Fizz"
+  else
+    nil
 end
